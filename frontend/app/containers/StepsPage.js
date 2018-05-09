6a0a7254a20b0846680a016d6a11bccc18e2c14d
Gielen Gunter <Gunter.Gielen@cegeka.com>
2018-05-09 14:58:33 +0200

    Frontend initial commit.

---
+++ frontend/app/containers/StepsPage.js
@@ -0,0 +1,15 @@
+// @flow
+import React, { Component } from 'react';
+import Steps from '../components/Steps';
+
+type Props = {};
+
+export default class StepsPage extends Component<Props> {
+  props: Props;
+
+  render() {
+    return (
+      <Steps />
+    );
+  }
+}
